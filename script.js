5c2ee77326ed34bd2fab63ef1c796f87725c8343
Macover <macover1507@hotmail.com>
2022-08-10 09:01:36 -0500

    problema 4

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// Problema 3
+// Problema 4
 
-const array1 = [1, 3, 3, 4, 5, 6, 8, 9, 9];
-const array2 = [2, 2, 3, 5, 7, 9, 9];
+const esUnTriangulo = (a, b, c) => {
 
-const quitarNumerosRepetidos = (array1, array2) => {
-    const newArray = [];
-    for (let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2.length; j++) {
-            if(array1[i] === array2[j]){
-                newArray.push(array1[i]);
-                delete array1[i];
-                delete array2[j];
-            }
+    const suma1 = a + b;
+    const suma2 = a + c;
+    const suma3 = b + c;
+
+    if(a < 0 || b < 0 || c < 0){
+        console.log("Error")
+    }else{
+        if(suma1 > c || suma2 > b || suma3 > a){
+            console.log("Se puede construir")
+        }else{
+            console.log("No se puede construir")
         }
     }
-    return newArray;
 }
-console.log(quitarNumerosRepetidos(array1,array2))
+esUnTriangulo(45,70,1)
