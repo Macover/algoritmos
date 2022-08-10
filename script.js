c9ed06a37cfbd9bc664309fa7f3117d1ce34fbd8
Macover <macover1507@hotmail.com>
2022-08-10 08:45:19 -0500

    problema 3

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-// Problema 2
+// Problema 3
 
-const array1 = [1, 2, 6, 8, 9];
-const array2 = [3, 5, 6, 9];
+const array1 = [1, 3, 3, 4, 5, 6, 8, 9, 9];
+const array2 = [2, 2, 3, 5, 7, 9, 9];
 
-const fucionaArreglosOrdenados = (array1, array2) => {
-    let newArray = [];
+const quitarNumerosRepetidos = (array1, array2) => {
+    const newArray = [];
     for (let i = 0; i < array1.length; i++) {
-        newArray.push(array1[i])
-    }
-    for (let i = 0; i < array2.length; i++) {
-        newArray.push(array2[i])
-    }
-    return ordenaArreglo(newArray)
-}
-const ordenaArreglo = array => {
-    const l = array.length;
-    for (let i = 0; i < l; i++) {
-        for (let j = 0; j < l - 1 - i; j++) {
-            if (array[j] > array[j + 1]) {
-                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        for (let j = 0; j < array2.length; j++) {
+            if(array1[i] === array2[j]){
+                newArray.push(array1[i]);
+                delete array1[i];
+                delete array2[j];
             }
         }
     }
-    return array;
-};
+    return newArray;
+}
+console.log(quitarNumerosRepetidos(array1,array2))
